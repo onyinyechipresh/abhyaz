5e4c6e716e7112bc72d0e8061b63b91a9f57cdf1
peaceonyinye <onyinyechipresh05@gmail.com>
2023-09-14 22:09:38 +0100

    modified dto

--- src/main/java/com/example/abhyaz/FeedbackDto.java
+++ src/main/java/com/example/abhyaz/FeedbackDto.java
@@ -1,8 +1,8 @@
 package com.example.abhyaz;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,8 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class FeedbackDto {
+    @NotEmpty(message = "name must not be empty")
     private String name;
+    @NotEmpty(message = "email cannot be empty")
+    @Email(message = "email must be a valid email address")
     private String emailAddress;
+    @NotEmpty(message = "subject must not be empty")
     private String subject;
+    @NotEmpty(message = "message must not be empty")
     private String message;
 }
