89d21486be41eb3fce2255f40fa33e9b1bc100c8
peaceonyinye <onyinyechipresh05@gmail.com>
2023-09-14 21:27:28 +0100

    modified controller

--- src/main/java/com/example/abhyaz/FeedbackController.java
+++ src/main/java/com/example/abhyaz/FeedbackController.java
@@ -3,14 +3,12 @@ package com.example.abhyaz;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/feedback")
+@CrossOrigin(origins = "http://localhost:3000")
 public class FeedbackController {
     private final FeedbackService feedbackService;
 
